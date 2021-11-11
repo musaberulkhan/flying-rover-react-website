d68fc2b53d9ead72e250a7b5e1af4e848fe752a5
Ahmed Musaberul Khan <musaberulkhan@gmail.com>
2021-11-11 21:26:56 +0600

    Footer Added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 import Products from '../Products/Products';
 import Banner from './Banner/Banner';
 import FeaturedProducts from './FeaturedProducts/FeaturedProducts';
+import './Home.css';
 
 const Home = () => {
     return (
-        <div>
+        <div className="home">
             <Header></Header>
             <Banner></Banner>
             <FeaturedProducts></FeaturedProducts>
+            <Footer></Footer>
         </div>
     );
 };
